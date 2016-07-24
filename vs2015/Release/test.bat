b2eb529d4b24defed31e1125497aef306a881efe
radioman <email@radioman.lt>
2016-07-24 22:42:33 +0300

    test

--- vs2015/Release/test.bat
+++ vs2015/Release/test.bat
@@ -1,4 +1,4 @@
-%~dp0\ConsoleApplication1.exe in.bmp out.bmp  0 0.5  0 3.5  1 14 5.0  1 500 500 0 0 0.0  0
+%~dp0\ConsoleApplication1.exe in.bmp out.bmp  0 0.5  0 3.5  1 30 5.0  1 1000 500 0 0 0.1  0
 
 rem ok
 
